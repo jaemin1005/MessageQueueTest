88e6a246190f0a1244e77ec1acef79f27929c354
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-29 19:54:11 +0900

    Feat | app : 라우터 추가
    Routers 디렉토리의 메인 라우터 연결

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import router from "./Routers/routers";
 
 const app = express();
+
+app.use(express.json());
+app.use('routers', router);
 
 app.listen(3000, () => {
   console.log("express : http://localhost:3000")
