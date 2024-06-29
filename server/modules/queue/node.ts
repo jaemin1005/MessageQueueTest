e074bbd47d4712cd0651098be9930cc228a33c17
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-29 15:14:57 +0900

    Fix | node - prpoerty next 타입 변경
    IQueueNode<T> => IQueueNode<T> | null 로 변경

--- server/modules/queue/node.ts
+++ server/modules/queue/node.ts
@@ -1,12 +1,12 @@
 export interface IQueueNode<T>{
   data : T
-  next : IQueueNode<T>
+  next : IQueueNode<T> | null;
 }
 
 export class QueueNode<T> implements IQueueNode<T>{
   data 
   next
-  constructor(data : T, next : QueueNode<T>){
+  constructor(data : T, next : QueueNode<T> | null ){
     this.data = data;
     this.next = next;
   }
