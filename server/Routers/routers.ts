da7c5dbd6c556de340e6cf3262d8d71b99f1202b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-29 20:25:10 +0900

    Feat | routers : /message에 대한 라우터를 추가
    /message에 대한 라우터를 추가

--- server/Routers/routers.ts
+++ server/Routers/routers.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import messageRouter from "./message"
 
 const router = Router();
+
+router.use('/message', messageRouter)
 
 router.get('/', (req, res) => {
   res.send("DEFAULT");
