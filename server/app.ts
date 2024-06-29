fa7947d8e406775df2068f80c35676aef71bfb24
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-29 20:02:42 +0900

    Fix | app : listen 메소드를 분리
    테스트를 위해 listen 메소드를 분리하겠음

--- server/app.ts
+++ server/app.ts
@@ -6,9 +6,9 @@ const app = express();
 app.use(express.json());
 app.use('routers', router);
 
-app.listen(3000, () => {
-  console.log("express : http://localhost:3000")
-  console.log("서버 구동 완료");
-})
+// app.listen(3000, () => {
+//   console.log("express : http://localhost:3000")
+//   console.log("서버 구동 완료");
+// })
 
 export default app
